eaa343acfad81c4c64fa74a214dcddf5c1a1813d
mokhallid80 <mokhallid72@gmail.com>
2023-11-26 14:09:44 +0200

    updated the drawer

--- lib/components/drawerWidget.dart
+++ lib/components/drawerWidget.dart
@@ -1,42 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            height: 100,
-            width: double.infinity,
-            padding: EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
-            color: Colors.orangeAccent,
-            child: Text(
-              'El Matbakh',
-              style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 30,
-                  color: Colors.white),
-            ),
+          Column(
+            children: [
+              Container(
+                height: 100,
+                width: double.infinity,
+                padding: EdgeInsets.all(20),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'elPlatform',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                      color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.phone),
+                title: Text("GUC Contacts"),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/');
+                },
+              ),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.mapLocation),
+                title: Text("Navigation"),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/');
+                },
+              ),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.book),
+                title: Text("Courses"),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/');
+                },
+              ),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.chalkboardUser),
+                title: Text("Instructors"),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/');
+                },
+              ),
+            ],
           ),
-          SizedBox(
-            height: 20,
-          ),
-          ListTile(
-            leading: Icon(Icons.fastfood_rounded),
-            title: Text("Aklat"),
-            onTap: () {
-              Navigator.of(context).pushNamed('/');
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text("Settings"),
-            onTap: () {
-              Navigator.of(context).pushNamed('/settingsRoute');
-            },
-          ),
+          Column(
+            children: [
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.rightFromBracket),
+                title: Text("Logout"),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/');
+                },
+              ),
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.info),
+                title: Text("About Us"),
+                onTap: () {
+                  Navigator.of(context).pushNamed('/');
+                },
+              ),
+            ],
+          )
         ],
       ),
     );
