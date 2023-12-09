b6a838637465b74999de5ed8b16d682be7ada0ee
Ziad Othman <ziadsadek999@gmail.com>
2023-12-09 22:57:20 +0200

    Course reviews display (#3)
    Made course reviews scrollable horizontally, created an over lay to expand the review and enhanced some styling.

--- lib/models/review.dart
+++ lib/models/review.dart
@@ -1,11 +1,11 @@
 class Review {
   final String userId;
-  final String review;
-  final double? rating;
+  final String? review;
+  final double rating;
 
   Review({
     required this.userId,
-    required this.review,
-    this.rating,
+    required this.rating,
+    this.review,
   });
 }
