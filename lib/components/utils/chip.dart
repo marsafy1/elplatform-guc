66ed519946efe21065968ec8dc0606c4db1bc71d
mokhallid80 <mokhallid72@gmail.com>
2023-12-12 12:23:05 +0200

    created the chip element

---
+++ lib/components/utils/chip.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../managers/categories_manager.dart';
+
+class ChipElement extends StatelessWidget {
+  final String category;
+  final categoriesMap = CategoryManager().categoriesMap;
+
+  ChipElement({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    var categoryItem = categoriesMap[category] ?? categoriesMap["all"];
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+      margin: const EdgeInsets.all(3.0),
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          FaIcon(categoryItem?.icon ?? FontAwesomeIcons.house, size: 10),
+          const SizedBox(width: 5),
+          Text(
+            categoryItem?.name ?? "Unknown $category",
+            style: const TextStyle(fontSize: 13),
+          ),
+        ],
+      ),
+    );
+  }
+}
