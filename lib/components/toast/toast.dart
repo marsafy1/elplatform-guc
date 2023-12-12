54ead98f35016a776764872f91d9777939f0b706
mokhallid80 <mokhallid72@gmail.com>
2023-12-12 16:33:01 +0200

    removing toast on tap

--- lib/components/toast/toast.dart
+++ lib/components/toast/toast.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Toast {
-  static void show(BuildContext context, String message, String type,
-      {IconData icon = FontAwesomeIcons.info, VoidCallback? onTap}) {
+  static void show(
+    BuildContext context,
+    String message,
+    String type, {
+    IconData icon = FontAwesomeIcons.info,
+    VoidCallback? onTap,
+  }) {
     Map<String, Color> typeToColor = {
       "success": Colors.green,
       "error": Colors.red,
@@ -15,27 +20,29 @@ class Toast {
     backgroundColor = backgroundColor.withOpacity(0.7);
 
     final overlay = Overlay.of(context);
-    final overlayEntry = OverlayEntry(
+
+    // Declare the overlayEntry variable
+    OverlayEntry? overlayEntry;
+
+    // Initialize the overlayEntry
+    overlayEntry = OverlayEntry(
       builder: (context) => Positioned(
-        top: MediaQuery.of(context).viewPadding.top +
-            100.0, // Positioning it below the status bar
-        width:
-            MediaQuery.of(context).size.width, // Match the width of the screen
+        top: MediaQuery.of(context).viewPadding.top + 100.0,
+        width: MediaQuery.of(context).size.width,
         child: GestureDetector(
           onTap: () {
             if (onTap != null) {
-              onTap(); // Execute the callback function if provided
+              onTap();
             }
             // Dismiss the toast when tapped
-            // if (overlayEntry.mounted) {
-            //   overlayEntry.remove();
-            // }
+            if (overlayEntry!.mounted) {
+              overlayEntry!.remove();
+            }
           },
           child: Center(
             child: Material(
               color: backgroundColor,
-              elevation:
-                  10.0, // Optional: gives the Material widget some shadow.
+              elevation: 10.0,
               borderRadius: const BorderRadius.all(Radius.circular(20)),
               child: Padding(
                 padding:
@@ -46,16 +53,14 @@ class Toast {
                     Container(
                       margin: const EdgeInsets.only(right: 5.0),
                       decoration: const BoxDecoration(
-                        shape: BoxShape.circle, // Makes the container circular
-                        color:
-                            Color.fromARGB(76, 255, 255, 255), // Circle color
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(76, 255, 255, 255),
                       ),
-                      padding: const EdgeInsets.all(
-                          10), // The padding inside the circle
+                      padding: const EdgeInsets.all(10),
                       child: FaIcon(
                         icon,
-                        size: 14, // Adjust icon size as needed
-                        color: Colors.white, // Icon color
+                        size: 14,
+                        color: Colors.white,
                       ),
                     ),
                     Text(
@@ -75,10 +80,9 @@ class Toast {
 
     overlay?.insert(overlayEntry);
 
-    // Automatically dismiss the notification after some duration
     Future.delayed(const Duration(seconds: 5)).then((value) {
-      if (overlayEntry.mounted) {
-        overlayEntry.remove();
+      if (overlayEntry?.mounted ?? false) {
+        overlayEntry?.remove();
       }
     });
   }
