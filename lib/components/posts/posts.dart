111efec0bf9e705ebaff97b71c04313a62380dc9
mokhallid80 <mokhallid72@gmail.com>
2023-12-12 12:55:33 +0200

    added posts container

--- lib/components/posts/posts.dart
+++ lib/components/posts/posts.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import '../../models/category.dart';
+import '../../models/post.dart';
+import './post.dart';
 
+class Posts extends StatelessWidget {
+  final List<Category> selectedCategories;
+  final List<Post> posts;
+  final ScrollController controller;
+
+  const Posts(
+      {super.key,
+      required this.posts,
+      required this.selectedCategories,
+      required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      controller: controller,
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: posts.map((post) {
+          return PostWidget(
+            post: post,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
