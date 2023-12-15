cb44acacc94aa5254ebd4b5929cf4eb6946ed763
mokhallid80 <mokhallid72@gmail.com>
2023-12-15 13:36:11 +0200

    added comments components

---
+++ lib/components/comments/comment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Comment extends StatelessWidget {
+  const Comment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("comment");
+  }
+}
---
+++ lib/components/comments/comments.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Comments extends StatefulWidget {
+  const Comments({super.key});
+
+  @override
+  State<Comments> createState() => _CommentsState();
+}
+
+class _CommentsState extends State<Comments> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('All comments');
+  }
+}
