21aff0d841e37085009d52c8076ea15ba8392335
mokhallid80 <mokhallid72@gmail.com>
2023-12-17 15:25:26 +0200

    added new field to posts

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -8,6 +8,7 @@ class Post {
   final String description;
   final DateTime dateCreated;
   final List<dynamic>? photosUrls;
+  final Set<String> likedByUsers; // New field to track likes
   User? user;
 
   Post(
@@ -18,21 +19,26 @@ class Post {
       required this.description,
       required this.dateCreated,
       this.photosUrls,
-      this.user});
+      this.user,
+      this.likedByUsers = const {} // Default to empty set
+      });
 
   factory Post.fromMap(Map<String, dynamic> map, String documentId,
       {User? user}) {
     return Post(
-        id: documentId,
-        title: map['title'] ?? "Title",
-        userId: map['userId'] ?? "Default",
-        category: map['category'] ?? "all",
-        description: map['description'] ?? "Description",
-        photosUrls: map['photosUrls'] ?? [],
-        dateCreated: map['dateCreated'] != null
-            ? map['dateCreated'].toDate()
-            : DateTime.now(),
-        user: user);
+      id: documentId,
+      title: map['title'] ?? "Title",
+      userId: map['userId'] ?? "Default",
+      category: map['category'] ?? "all",
+      description: map['description'] ?? "Description",
+      photosUrls: map['photosUrls'] ?? [],
+      dateCreated: map['dateCreated'] != null
+          ? map['dateCreated'].toDate()
+          : DateTime.now(),
+      user: user,
+      likedByUsers:
+          Set<String>.from(map['likedByUsers'] ?? []), // Extract likes
+    );
   }
 
   Map<String, dynamic> toMap() {
@@ -43,6 +49,7 @@ class Post {
       'description': description,
       'photosUrls': photosUrls,
       'dateCreated': dateCreated,
+      'likedByUsers': likedByUsers.toList(), // Convert to list for storage
     };
   }
 }
