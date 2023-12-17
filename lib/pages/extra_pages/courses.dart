bc68693e28124b1c7427c367ae7f999f6af4c2d9
Ziad Othman <ziadsadek999@gmail.com>
2023-12-18 01:49:40 +0200

    Search for courses and instructors (#29)

--- lib/pages/extra_pages/courses.dart
+++ lib/pages/extra_pages/courses.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:guc_swiss_knife/configs/constants.dart';
 import 'package:guc_swiss_knife/models/course.dart';
 import 'package:guc_swiss_knife/services/course_service.dart';
-
 import '../../components/course/course_card.dart';
 
 class Courses extends StatefulWidget {
@@ -21,6 +20,7 @@ class _CoursesState extends State<Courses> {
 
   @override
   Widget build(BuildContext context) {
+    var keyBoardHeight = MediaQuery.of(context).viewInsets.bottom;
     return Scaffold(
         appBar: AppBar(
           title: const Text("$appName - Courses"),
@@ -49,17 +49,47 @@ class _CoursesState extends State<Courses> {
                 return Text('Error: ${snapshot.error}');
               } else {
                 List<Course>? courses = snapshot.data;
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: courses!.length,
-                    itemBuilder: (context, index) {
-                      return CourseCard(course: courses[index]);
-                    },
+                return Column(children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(12, 12, 12, 12),
+                    child: SearchAnchor.bar(
+                      barHintText: 'Search courses',
+                      suggestionsBuilder:
+                          (BuildContext context, SearchController controller) {
+                        return getSuggestions(controller, courses!);
+                      },
+                    ),
                   ),
-                );
+                  SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.8 -
+                          keyBoardHeight,
+                      child: ListView.builder(
+                        itemCount: courses!.length,
+                        shrinkWrap: true,
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        itemBuilder: (context, index) {
+                          return CourseCard(course: courses[index]);
+                        },
+                      ))
+                ]);
               }
             },
           ),
         ]));
+  }
+
+  Iterable<Widget> getSuggestions(
+      SearchController controller, List<Course> courses) {
+    final String input = controller.value.text.toLowerCase();
+    return courses
+        .where((Course course) => course.title.toLowerCase().contains(input))
+        .map(
+          (Course filteredCourse) => CourseCard(course: filteredCourse),
+        );
+  }
+
+  void handleSelection(Course selectedCourse) {
+    Navigator.of(context)
+        .pushNamed('/courseDetails', arguments: {'instructor': selectedCourse});
   }
 }
