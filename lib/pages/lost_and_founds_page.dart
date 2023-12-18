5fddc37d2c8853f3960e31d21e5420ac313db24c
mokhallid80 <mokhallid72@gmail.com>
2023-12-18 18:15:45 +0200

    added categories

--- lib/pages/lost_and_founds_page.dart
+++ lib/pages/lost_and_founds_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:guc_swiss_knife/services/route_observer_service.dart';
+import '../models/category.dart';
 import '../models/post.dart';
+import '../managers/lost_and_founds_manager.dart';
 import '../components/posts/posts.dart';
+import '../components/categories/category_icon.dart';
+import '../components/categories/categories.dart';
 
 import '../services/posts_service.dart';
 import '../components/toast/toast.dart';
+
 import '../components/utils/no_content.dart';
 
 class LostAndFoundsPage extends StatefulWidget {
@@ -18,19 +23,94 @@ class LostAndFoundsPage extends StatefulWidget {
 class _LostAndFoundsPageState extends State<LostAndFoundsPage> {
   final PostsService _postsService = PostsService();
 
+  late List<Category> categories;
+  List<Category> selectedCategories = [];
+
+  late List<Category> categoriesChoices;
+  List<Category> selectedCategoriesChoices = [];
+
+  var categoriesMap = LostAndFoundsManager().categoriesMap;
+
   DateTime? latestPostTimestamp;
   bool hasNewPosts = false;
 
-  bool lostsOnly = false;
-
-  final ScrollController _scrollController = ScrollController();
+  bool answeredOnly = false;
 
   @override
   void initState() {
     RouteObserverService().logUserActivity('/lost_and_founds');
     super.initState();
+    categories = categoriesMap.entries.map((entry) {
+      return Category(
+        name: entry.value.name, // Use the name from the CategoryItem in the map
+        icon: CategoryIcon(
+            icon: entry.value.icon), // Use the icon from the CategoryItem
+        addCategory: addCategory,
+        removeCategory: removeCategory,
+      );
+    }).toList();
+
+    categoriesChoices = categoriesMap.entries.map((entry) {
+      return Category(
+        name: entry.value.name, // Use the name from the CategoryItem in the map
+        icon: CategoryIcon(
+            icon: entry.value.icon), // Use the icon from the CategoryItem
+        addCategory: addCategory,
+        removeCategory: removeCategory,
+      );
+    }).toList();
+
+    setState(() {
+      categories[0].selected = true;
+      categoriesChoices[0].selected = true;
+    });
   }
 
+  // void updateCategoriesAndReopenBottomSheet() {
+  //   Navigator.pop(context); // Close the existing bottom sheet
+  //   someFunctionThatUpdatesCategories(); // Update your categoriesChoices here
+  //   showBottomSheetForNewPost(context); // Reopen the bottom sheet
+  // }
+
+  void addCategory(Category c, bool asFilter) {
+    setState(() {
+      if (asFilter) {
+        selectedCategories.clear();
+        if (c.name.toLowerCase() != "all") {
+          selectedCategories.add(c);
+          print('Added category: ${c.name}');
+        }
+        categories.forEach((element) {
+          element.selected = false;
+        });
+        c.selected = true;
+      }
+    });
+
+    categories.forEach((element) {
+      print(element.name + " " + element.selected.toString());
+    });
+    selectedCategories.forEach((element) {
+      print("SS " + element.name + " " + element.selected.toString());
+    });
+  }
+
+  void removeCategory(Category c, bool asFilter) {
+    setState(() {
+      print("remove category");
+      if (asFilter) {
+        selectedCategories.remove(c);
+        print("checking if category is empty");
+        if (selectedCategories.isEmpty) {
+          print("empty");
+          categories[0].selected = true;
+          addCategory(categories[0], asFilter);
+        }
+      }
+    });
+  }
+
+  final ScrollController _scrollController = ScrollController();
   @override
   void dispose() {
     _scrollController.dispose(); // Important to dispose the controller
@@ -48,7 +128,7 @@ class _LostAndFoundsPageState extends State<LostAndFoundsPage> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: _postsService.getPosts('lost_and_founds'),
+      stream: _postsService.getPosts('questions'),
       builder: (context, AsyncSnapshot<List<Post>> snapshot) {
         if (snapshot.hasData) {
           if (snapshot.data!.isNotEmpty) {
@@ -76,20 +156,23 @@ class _LostAndFoundsPageState extends State<LostAndFoundsPage> {
         }
         // Retrieve posts and filter them based on selected categories
         List<Post> allPosts = snapshot.data ?? [];
-        List<Post> filteredPosts = [];
-        if (lostsOnly) {
-          allPosts.forEach((element) {
-            if (element.resolved == false) {
-              filteredPosts.add(element);
-            }
-          });
-        } else {
-          filteredPosts = allPosts;
+        List<Post> filteredPosts = selectedCategories.isEmpty
+            ? allPosts
+            : allPosts.where((post) {
+                return selectedCategories.any((category) =>
+                    post.category.toLowerCase() == category.name.toLowerCase());
+              }).toList();
+        if (answeredOnly) {
+          filteredPosts = filteredPosts.where((post) {
+            return post.resolved == true;
+          }).toList();
         }
 
         if (snapshot.data!.isNotEmpty) {
           return Column(
             children: [
+              Categories(
+                  key: UniqueKey(), categories: categories, asFilter: true),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
@@ -97,24 +180,27 @@ class _LostAndFoundsPageState extends State<LostAndFoundsPage> {
                       style:
                           TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
                   Switch(
-                    value: lostsOnly,
+                    value: answeredOnly,
                     activeColor: Theme.of(context).colorScheme.primary,
                     onChanged: (bool value) {
                       setState(() {
-                        lostsOnly = value;
+                        answeredOnly = value;
                       });
                     },
                   ),
                 ],
               ),
-              Expanded(
-                child: Posts(
-                  posts: filteredPosts,
-                  selectedCategories: const [],
-                  controller: _scrollController,
-                  collection: "lost_and_founds",
+              if (filteredPosts.isNotEmpty)
+                Expanded(
+                  child: Posts(
+                    posts: filteredPosts,
+                    selectedCategories: selectedCategories,
+                    controller: _scrollController,
+                    collection: "lost_and_founds",
+                  ),
                 ),
-              ),
+              if (!filteredPosts.isNotEmpty)
+                const NoContent(text: "No Posts Available")
             ],
           );
         } else {
